98c91f0ac6df0747e34b1fd4c9d541b4fb6f92f7
MaheshGadkar33 <gkmahi33.com>
2026-05-08 20:30:56 +0530

    Episode-03

--- App.js
+++ App.js
@@ -1,16 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "This is h1 tag"),
-    React.createElement("h2", {}, "This is h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "This is h1 tag"),
-    React.createElement("h2", {}, "This is h2 tag"),
-  ]),
-]);
-console.log(parent);
+//using React
+const heading = React.createElement("h1", {}, "Namaste React 🧨");
+
+const element = <span>This is React Element 🔥</span>;
+
+//React Component
+//class Based Component-old
+//Functional Component-new
+//this is also valid without return statement
+const HedaingComponent2 = () => <div>This is a function component </div>;
+
+//using JSX(React Element)
+const jsxheading = (
+  <h1 className="heading">
+    {element}Namaste React 🧨 using JSX
+    <HedaingComponent2 />
+  </h1>
+);
+//component composition => Render one component in another
+const HedaingComponent = () => {
+  return (
+    <div>
+      <HedaingComponent2 />
+      {jsxheading}
+      <h1>This is a function component ✅ with return</h1>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+// root.render(jsxheading);
+root.render(<HedaingComponent />);
+
+// console.log(heading);
+// console.log(jsxheading);
